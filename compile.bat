14f1d61a400392e7b64a4af60d40fd4f36e5c109
lutischan-ferenc <lutischanf@gmail.com>
2025-09-18 09:36:37 +0200

    Fix Windows Defender invalid virus action

--- compile.bat
+++ compile.bat
@@ -1,2 +1,2 @@
 ..\mingw32\bin\windres TrayMp3.rc -o TrayMp3_res.o
-..\mingw32\bin\gcc -Os -s -ffunction-sections -fdata-sections -fno-exceptions -Wl,--gc-sections -Wl,--strip-all -Wl,--major-image-version,4 -Wl,--minor-image-version,0 -mwindows -I. -o TrayMp3 TrayMp3.c TrayMp3_res.o -lkernel32 -luser32 -lgdi32 -lwinmm -lcomctl32 -lshlwapi -lshell32 -lole32 -lpropsys -lmf -lmfplat -lmfreadwrite -static-libgcc
+..\mingw32\bin\gcc -Os -s -ffunction-sections -fdata-sections -fno-exceptions -Wl,--gc-sections -Wl,--strip-all -mwindows -I. -o TrayMp3 TrayMp3.c TrayMp3_res.o -lkernel32 -luser32 -lgdi32 -lwinmm -lcomctl32 -lshlwapi -lshell32 -lole32 -lpropsys -lmf -lmfplat -lmfreadwrite -static-libgcc
